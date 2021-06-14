21fb6dd1eda288978b5162928e5e7b136c77fd38
Ci3l <80896731+Ci3l@users.noreply.github.com>
2021-06-14 15:30:06 +0200

    màj

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from array import *
 import random
 
 def init():
-    for x in map:
-        for y in x:
-            print( str(y) + " ",end='')
-            y = random.randint(0, 10)
-            print(str(y))
+    for x in range(length):
+        for y in range(width):
+            map[x][y] = random.randint(1,10)
 
 length, width = 10, 10
-map = [[0 for x in range(length)] for y in range(width)]
+map = [[0 for x in range(length)] for y in range(width)] 
 init()
 print(map)
