44973df4890357b32e4288b7aa636ee303298452
Rooomyy <romane6273@gmail.com>
2021-06-14 20:02:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def setPixel(x, y, newValue):
 def getPixel(x, y):
     return map[x][y]
 
+def printMap():
+    print()
+
+
 length, width = 10, 10
 map = [[0 for x in range(length)] for y in range(width)] 
 init()
