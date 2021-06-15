7ce7f2c8165474c7b122ca811c62020b130e39a2
Ci3l <80896731+Ci3l@users.noreply.github.com>
2021-06-15 21:05:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from array import *
 import random
+import os
+import time
+
+length, width = 30, 30
+map = [[0 for x in range(length)] for y in range(width)]
 
 def initMap():
     for x in range(length):
         for y in range(width):
-            map[x][y] = random.randint(0,9)
+            map[x][y] = random.randint(0,1)
 
 def printMap():
    for x in range(length):
@@ -12,36 +17,52 @@ def printMap():
             print(' ' + str( map[x][y]),end= '')
         print()
 
-def logic():
+def logic(map):
     newMap = [[0 for x in range(length)] for y in range(width)]
     for x in range(length):
         for y in range(width):
             num = []
-            if x == 0:
+            if x == 0 and y == 0 :#en haut à gauche
+                num.append(map[x][y+1])
+                num.append(map[x+1][y+1])
+                num.append(map[x+1][y])
+            if x == 0 and y == width-1 :#en haut à droite 
+                num.append(map[x+1][y])
+                num.append(map[x+1][y-1])
+                num.append(map[x][y-1])
+            if x == length-1 and y == width-1 :#en bas à droite
+                num.append(map[x][y-1])
+                num.append(map[x-1][y-1])
+                num.append(map[x-1][y])
+            if x == length-1 and y == 0: #en bas à gauche
+                num.append(map[x-1][y])
+                num.append(map[x-1][y+1])
+                num.append(map[x][y+1])
+            elif x == 0 and y != 0 and y != width - 1:#première ligne
+                num.append(map[x][y+1])
+                num.append(map[x+1][y+1])
+                num.append(map[x+1][y])
+                num.append(map[x+1][y-1])
+                num.append(map[x][y-1])
+            elif y == 0 and x != 0 and x != length -1:#première colonne
+                num.append(map[x-1][y])
                 num.append(map[x-1][y+1])
                 num.append(map[x][y+1])
                 num.append(map[x+1][y+1])
                 num.append(map[x+1][y])
-                num.append(map[x-1][y])
-            if x == length:
-                num.append(map[x+1][y])
-                num.append(map[x+1][y-1])
-                num.append(map[x][y-1])
-                num.append(map[x-1][y-1])
-                num.append(map[x-1][y])
-            if y == 0:
+            elif x == width-1 and y != 0 and y != width - 1:#dernière ligne
                 num.append(map[x][y+1])
-                num.append(map[x+1][y+1])
-                num.append(map[x+1][y])
-                num.append(map[x+1][y-1])
-                num.append(map[x][y-1])
-            if y == width:
                 num.append(map[x-1][y+1])
-                num.append(map[x][y+1])
-                num.append(map[x][y-1])
-                num.append(map[x-1][y-1])
                 num.append(map[x-1][y])
-            else:
+                num.append(map[x-1][y-1])
+                num.append(map[x][y-1])
+            elif y == length -1 and x != 0 and x != length - 1:#dernière colonne
+                num.append(map[x+1][y])
+                num.append(map[x-1][y])
+                num.append(map[x-1][y-1])
+                num.append(map[x][y-1])
+                num.append(map[x+1][y-1])
+            elif y != 0 and y != width-1 and x != 0 and x != length-1:
                 num.append(map[x-1][y+1])
                 num.append(map[x][y+1])
                 num.append(map[x+1][y+1])
@@ -52,18 +73,23 @@ def logic():
                 num.append(map[x-1][y])
             livingCells = num.count(1)
             diedCells = num.count(0)
-            if livingCells == 3:
+            if livingCells == 3 :
                 newMap[x][y] = 1
-            if diedCells != 2 or diedCells != 3:
+            if livingCells == 2 and map[x][y] == 1:
+                newMap[x][y] = 1
+            elif livingCells != 2 and livingCells != 3:
                 newMap[x][y] = 0
-    map = newMap
+    for x in range(length):
+        for y in range(width):
+            map[x][y] = newMap[x][y]
 
-length, width = 10, 10
-map = [[0 for x in range(length)] for y in range(width)] 
 initMap()
-printMap()
-logic()
-printMap()
-
-
-
+generations = input()
+i = 0
+while i <= int(generations) :
+    time.sleep(1.16)
+    os.system('cls')
+    logic(map)
+    print()
+    printMap()
+    i += 1 
